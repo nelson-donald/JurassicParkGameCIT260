5214b4dee732ee4cbf36b03f3d60b196a27b0ab7
igadg <igadg@is1506424.shcsd.sharp.com>
2016-05-10 22:34:43 -0700

    Finished the Main fuction

--- JurassicParkGameCIT260/src/jurassicparkgamecit260/JurassicParkGameCIT260.java
+++ JurassicParkGameCIT260/src/jurassicparkgamecit260/JurassicParkGameCIT260.java
@@ -5,6 +5,9 @@
  */
 package jurassicparkgamecit260;
 
+import byui.cit260.jurassicpark.model.Inventory;
+import byui.cit260.jurassicpark.model.Player;
+
 /**
  *
  * @author igadg
@@ -16,6 +19,13 @@ public class JurassicParkGameCIT260 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Player p = new Player();
+        p.setName("The Best Group. Pretty humble too.");
+        p.setDirection(1);
+        p.setInventory(new Inventory());
+        p.setLocation(new String[1][1]);
+        
+        System.out.println(p.toString());
     }
     
 }
