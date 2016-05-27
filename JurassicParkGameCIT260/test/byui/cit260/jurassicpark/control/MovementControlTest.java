30092c94fc6cc86d34adfe5e479924f0d93be87c
igadg <igadg@is1506424.shcsd.sharp.com>
2016-05-26 17:47:06 -0700

    created test case

--- JurassicParkGameCIT260/test/byui/cit260/jurassicpark/control/MovementControlTest.java
+++ JurassicParkGameCIT260/test/byui/cit260/jurassicpark/control/MovementControlTest.java
@@ -27,6 +27,8 @@ public class MovementControlTest {
         Dinosaur d = new Dinosaur();
         MovementControl instance = new MovementControl();
         
+        //**********************************************************************
+        //TEST 1
         //These are the input values.
         int tourists = 2;
         d.setPowerLevel(1);
@@ -35,6 +37,31 @@ public class MovementControlTest {
         int expResult = 1;
         
         int result = instance.dinoEncounter(tourists, d);
+        assertEquals(expResult, result);
+        
+        //**********************************************************************
+        //TEST 2
+        //These are the input values.
+        tourists = 8;
+        d.setPowerLevel(2);
+        
+        //This is the output value
+        expResult = 6;
+        
+        result = instance.dinoEncounter(tourists, d);
+        assertEquals(expResult, result);
+        
+        
+        //**********************************************************************
+        //TEST 3
+        //These are the input values.
+        tourists = 1;
+        d.setPowerLevel(3);
+        
+        //This is the output value
+        expResult = -10;
+        
+        result = instance.dinoEncounter(tourists, d);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
