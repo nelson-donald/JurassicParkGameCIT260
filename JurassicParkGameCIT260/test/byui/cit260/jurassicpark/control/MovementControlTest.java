da97bdbbe9bfb7f237a50b34aa8d38cab74504fa
igadg <igadg@is1506424.shcsd.sharp.com>
2016-05-26 17:40:57 -0700

    created test case

---
+++ JurassicParkGameCIT260/test/byui/cit260/jurassicpark/control/MovementControlTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.jurassicpark.control;
+
+import byui.cit260.jurassicpark.model.Dinosaur;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author igadg
+ */
+public class MovementControlTest {
+    
+    public MovementControlTest() {
+    }
+
+    /**
+     * Test of dinoEncounter method, of class MovementControl.
+     */
+    @Test
+    public void testDinoEncounter() {
+        System.out.println("dinoEncounter");
+        Dinosaur d = new Dinosaur();
+        MovementControl instance = new MovementControl();
+        
+        //These are the input values.
+        int tourists = 2;
+        d.setPowerLevel(1);
+        
+        //This is the output value
+        int expResult = 1;
+        
+        int result = instance.dinoEncounter(tourists, d);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
