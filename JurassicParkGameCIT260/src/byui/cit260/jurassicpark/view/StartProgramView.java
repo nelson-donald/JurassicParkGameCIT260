d3c8c73dde88f62e2f45a3222e3bf79a3164f080
joeyhendrix <joeyhendrix@joeys-air.fios-router.home>
2016-06-02 17:43:11 -0700

    I added some text to the welcome output.

--- JurassicParkGameCIT260/src/byui/cit260/jurassicpark/view/StartProgramView.java
+++ JurassicParkGameCIT260/src/byui/cit260/jurassicpark/view/StartProgramView.java
@@ -88,8 +88,29 @@ public class StartProgramView {
         welcome = "=================================================" +
                 "\nWelcome to Jurassic Park" +
                 "\n" +
-                "\nSomeone has sabotoged the park and we need your" +
-                "\nhelp to get it back online and get escape." +
+                "\nFor almost a year now, people from all over the" +
+                "\nworld have visited our park to see these spectacular" +
+                "\ncreatures who have been extinct for millions of years." +
+                "\n" +
+                "\nEverything has run smoothly, until today." +
+                "\n" +
+                "\nYou are a newly hired tour guide on your first day" +
+                "\nwho experiences one of the most horrific things" +
+                "\nmankind has ever seen before." +
+                "\n" +
+                "\nThere has been a massive power outage on the island" +
+                "\nand thousands of people, including you, are in panic." +
+                "\n" +
+                "\nCages can no longer hold these dinosaurs and a handful" +
+                "\nof them have already found their way out." +
+                "\n" +
+                "\nYou are one of the last tour guides who will need" +
+                "\nto find the power grid and get back online while" +
+                "\nhelping others to safety." +
+                "\n" +
+                "\nGood luck and be careful out there! A T-Rex has already" +
+                "\nbeen reported missing." +
+                "\n" +
                 "\n=================================================";
                 
         
